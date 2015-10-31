7ea164bfbd338c2681a6024de8c01d1f7f1259f6
IniOluwa Fageyinbo <inioluwafageyinbo@gmail.com>
2015-10-31 11:36:26 +0100

    Folder structure for bucketlist-api constructed.

---
+++ app/models.py
@@ -0,0 +1,51 @@
+import os
+import datetime
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+db = SQLAlchemy(app)
+
+
+class Base(db.Models):
+    __abstract__ = True
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(default=datetime.now())
+
+
+class User(Base):
+    __tablename__ = 'user'
+    username = db.Column(db.String(64), unique=True, index=True)
+    # password = 
+    user_bucketlists = db.relationship('BucketList', lazy='immediate')
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class BucketList(Base):
+    __tablename__ = 'bucketlist'
+    name = db.Column(db.String(64), unique=True, index=True)
+    items = db.relationship('BucketListItem', lazy='immediate')
+    date_modified = db.Column(default=datetime.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<BucketList %r>' % self.name
+
+
+class BucketListItem(Base):
+    __tablename__ = 'bucketlistitem'
+    name = db.Column(db.String, unique=True, index=True)
+    date_modified = db.Column(default=datetime.now())
+    done = db.Column(db.Boolean, default=False)
+    bucketlist_id = db.Column(db.Integer, db.ForeignKey('bucketlist.id'))
+
+    def __repr__(self):
+        return '<BucketListItem %r>' % self.name
