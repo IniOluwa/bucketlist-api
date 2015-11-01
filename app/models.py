f014c7f3e623b8e911fcd219d835b8aa5f73df08
IniOluwa Fageyinbo <inioluwafageyinbo@gmail.com>
2015-11-01 18:30:54 +0100

    Requirements, Manage, Models and Config updated

--- app/models.py
+++ app/models.py
@@ -1,30 +1,25 @@
 from . import db
+from passlib.apps import custom_app_context as password_context
 from datetime import datetime
-# from os import os
-# from flask import Flask
-# from flask.ext.sqlalchemy import SQLAlchemy
-
-# basedir = os.path.abspath(os.path.dirname(__file__))
-
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] =\
-#     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-# app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-
-# db = SQLAlchemy(app)
 
 
 class Base(db.Models):
     __abstract__ = True
     id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(default=datetime.utcnow())
+    date_created = db.Column(db.DateTime, default=datetime.now())
 
 
 class User(Base):
     __tablename__ = 'user'
     username = db.Column(db.String(64), unique=True, index=True)
-    # password =
+    user_password = db.Column(db.String(64))
     user_bucketlists = db.relationship('BucketList', lazy='immediate')
+
+    def hash_password(self, password):
+        self.user_password = password_context.encrypt(password)
+
+    def verify_password(self, password):
+        return password_context(password, self.user_password)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -34,7 +29,7 @@ class BucketList(Base):
     __tablename__ = 'bucketlist'
     name = db.Column(db.String(64), unique=True, index=True)
     items = db.relationship('BucketListItem', lazy='immediate')
-    date_modified = db.Column(default=datetime.utcnow())
+    date_modified = db.Column(db.DateTime, default=datetime.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
@@ -44,7 +39,7 @@ class BucketList(Base):
 class BucketListItem(Base):
     __tablename__ = 'bucketlistitem'
     name = db.Column(db.String, unique=True, index=True)
-    date_modified = db.Column(default=datetime.utcnow())
+    date_modified = db.Column(db.DateTime, datetime.now())
     done = db.Column(db.Boolean, default=False)
     bucketlist_id = db.Column(db.Integer, db.ForeignKey('bucketlist.id'))
 
--- manage.py
+++ manage.py
@@ -6,10 +6,17 @@ from flask.ext.migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
+migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, BucketList=BucketList, BucketListItem=BucketListItem)
+    return dict(
+        app=app,
+        db=db,
+        User=User,
+        BucketList=BucketList,
+        BucketListItem=BucketListItem
+        )
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
